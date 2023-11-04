afbe25c922aca4365129b0ba170c2543cb528e1c
Ruslan Dorogin <dorogin.ruslan99@yandex.ru>
2023-11-04 11:59:00 +0300

    Init commit

---
+++ src/main/java/ru/era/distributionoftasks/DistributionOfTasksApplication.java
@@ -0,0 +1,13 @@
+package ru.era.distributionoftasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributionOfTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributionOfTasksApplication.class, args);
+	}
+
+}
