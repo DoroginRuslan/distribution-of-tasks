342c24f29ada18ecc842273485cddbac4d3bf3ff
Ruslan Dorogin <dorogin.ruslan99@yandex.ru>
2023-11-05 10:42:37 +0300

    Добавлена авторизация, консоль БД отключена

--- src/main/java/ru/era/distributionoftasks/controllers/HelloWorldController.java
+++ src/main/java/ru/era/distributionoftasks/controllers/HelloWorldController.java
@@ -13,11 +13,6 @@ public class HelloWorldController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello World";
-    }
-
     @GetMapping("/addEmployee")
     public String addEmployee() {
         Employee employee = new Employee()
