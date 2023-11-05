fdcd6ca63ea1912a641156bbc1fe8a9d50d4c147
Ruslan Dorogin <dorogin.ruslan99@yandex.ru>
2023-11-05 12:26:50 +0300

    Добавлены странички для менеджера и соотрудника

--- src/main/java/ru/era/distributionoftasks/controllers/HomePageController.java
+++ src/main/java/ru/era/distributionoftasks/controllers/HomePageController.java
@@ -2,21 +2,19 @@ package ru.era.distributionoftasks.controllers;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomePageController {
     @GetMapping("/userpage")
-    @ResponseBody
-    public String userPageAuth(@AuthenticationPrincipal User user) {
-        return user.getUsername();
+    public String userPageAuth() {
+        return "userpage";
     }
 
     @GetMapping("/managerpage")
-    @ResponseBody
     public String managerPageAuth(@AuthenticationPrincipal User user) {
-        return user.getUsername();
+        return "managerpage";
     }
 }
