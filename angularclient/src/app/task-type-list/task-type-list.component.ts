7872653acf8c943edf343b0da2ac0eb266118c91
soroc <sorochanm@bk.ru>
2023-11-07 15:18:09 +0300

    Add TaskType

---
+++ angularclient/src/app/task-type-list/task-type-list.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { TaskType } from '../task-type';
+import { TaskTypeService } from '../task-type-service.service';
+
+@Component({
+  selector: 'app-taskType-list',
+  templateUrl: './task-type-list.component.html',
+  styleUrls: ['./task-type-list.component.css']
+})
+
+export class TaskTypeListComponent{
+
+  tasksType: TaskType[];
+
+  constructor(private taskTypeService: TaskTypeService) {
+  }
+
+  ngOnInit() {
+    this.taskTypeService.findAll().subscribe(data => {
+      this.tasksType = data;
+    });
+  }
+}
---
+++ angularclient/src/app/task-type-service.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { TaskType } from './task-type';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class TaskTypeService {
+
+  private tasksTypeUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.tasksTypeUrl = 'http://localhost:8080/getTasksType';
+  }
+
+  public findAll(): Observable<TaskType[]> {
+    return this.http.get<TaskType[]>(this.tasksTypeUrl);
+  }
+
+  public save(tasksType: TaskType) {
+    return this.http.post<TaskType>(this.tasksTypeUrl, tasksType);
+  }
+}
