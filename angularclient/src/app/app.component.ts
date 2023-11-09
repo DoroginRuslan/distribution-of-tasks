4258f3e45e9f20136e6d053924e0106f68b256b6
soroc <sorochanm@bk.ru>
2023-11-09 11:12:27 +0300

    add menu choosing

--- angularclient/src/app/app.component.ts
+++ angularclient/src/app/app.component.ts
@@ -12,14 +12,18 @@ import { finalize } from "rxjs/operators";
 export class AppComponent {
 
   title: string;
+  is_login: number; // not login, 1 - manager, 2 - employee
+
   constructor(private app: AppService, private http: HttpClient, private router: Router) {
         this.app.authenticate(undefined, undefined);
         this.title = 'Spring Boot - Angular Application';
+        this.is_login = 2;
       }
       logout() {
         this.http.post('logout', {}).pipe(
             finalize(() => {
             this.app.authenticated = false;
+            this.is_login = 0;
             this.router.navigateByUrl('/login');
         })).subscribe();
       }
