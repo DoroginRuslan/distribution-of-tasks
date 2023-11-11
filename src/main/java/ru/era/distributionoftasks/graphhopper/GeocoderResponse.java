11359bcdc43c20e125ceb88549fbcf903e38e1c8
soroc <sorochanm@bk.ru>
2023-11-11 12:55:35 +0300

    add YandexGeocoderService

---
+++ src/main/java/ru/era/distributionoftasks/graphhopper/GeocoderResponse.java
@@ -0,0 +1,46 @@
+package ru.era.distributionoftasks.graphhopper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeocoderResponse {
+    private List<GeoObject> geoObjects = new ArrayList<GeoObject>();
+    private String request;
+    private int found;
+    private int results;
+
+    public GeocoderResponse() {
+    }
+
+    public void setRequest(String request) {
+        this.request = request;
+    }
+
+    public void setFound(int found) {
+        this.found = found;
+    }
+
+    public void addGeoObject(GeoObject geoObject){
+        geoObjects.add(geoObject);
+    }
+
+    public List<GeoObject> getGeoObjects() {
+        return geoObjects;
+    }
+
+    public String getRequest() {
+        return request;
+    }
+
+    public int getFound() {
+        return found;
+    }
+
+    public int getResults() {
+        return results;
+    }
+
+    public void setResults(int results) {
+        this.results = results;
+    }
+}
---
+++ src/main/java/ru/era/distributionoftasks/graphhopper/YandexGeocoderService.java
@@ -0,0 +1,45 @@
+package ru.era.distributionoftasks.graphhopper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import java.io.IOException;
+
+@Service
+public class YandexGeocoderService {
+
+    public List<List<Double>> convertToCoordinates(List<String> adressArray)
+    {
+        List<List<Double>> allConvertedAdress = new ArrayList<List<Double>>();
+        for (String curAdress : adressArray)
+        {
+            allConvertedAdress.add(convertToCoordinate(curAdress));
+        }
+        return allConvertedAdress;
+    }
+
+    public List<Double> convertToCoordinate(String adress)
+    {
+        return sendRequestForConverting(adress);
+    }
+
+    public List<Double> sendRequestForConverting(String adress)
+    {
+        YaGeocoder geocoder = new YaGeocoder(new DefaultHttpClient());
+        GeocoderResponse response = null;
+        try {
+            response = geocoder.directGeocode(adress);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        GeoObject geoObject = response.getGeoObjects().get(0);
+        List<Double> convertedResult = new ArrayList<Double>();
+        convertedResult.add(geoObject.getPoint().lon);
+        convertedResult.add(geoObject.getPoint().lat);
+        return convertedResult;
+    }
+}
