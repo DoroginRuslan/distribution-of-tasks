a1c50f31ca358f8bb52d2bfa192124e702ce93b6
egor <galancc@mail.ru>
2023-11-13 11:24:09 +0300

    fixes

--- angularclient/src/app/grade-service.service.spec.ts
+++ angularclient/src/app/grade-service.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GradeServiceService } from './grade-service.service';
+import { GradeService} from './grade-service.service';
 
 describe('GradeServiceService', () => {
-  let service: GradeServiceService;
+  let service: GradeService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(GradeServiceService);
+    service = TestBed.inject(GradeService);
   });
 
   it('should be created', () => {
