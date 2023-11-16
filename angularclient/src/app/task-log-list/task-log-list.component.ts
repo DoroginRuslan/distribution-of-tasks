1a10d37c734ee64202e5ad6a90b049a29c43c379
JijaPog <macarows@mail.ru>
2023-11-16 15:28:43 +0300

    Прототип Отчёта в xlsx формате

--- angularclient/src/app/task-log-list/task-log-list.component.ts
+++ angularclient/src/app/task-log-list/task-log-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit  } from '@angular/core';
 import { TaskLog } from '../task-log';
 import { TaskLogService } from '../task-log-service.service';
+import * as XLSX from 'xlsx';
+import * as FileSaver from 'file-saver';
 
 @Component({
   selector: 'app-task-log-list',
@@ -8,7 +10,9 @@ import { TaskLogService } from '../task-log-service.service';
   styleUrls: ['./task-log-list.component.css']
 })
 
+
 export class TaskLogListComponent implements OnInit {
+  fileName= 'Отчёт.xlsx';
   taskLogs: TaskLog[];
   tasksFormed: boolean = false;
   loading: boolean = false;
@@ -18,19 +22,39 @@ export class TaskLogListComponent implements OnInit {
     ngOnInit() {
       this.taskLogService.findAll().subscribe(data => {
         this.taskLogs = data;
+
+
+
         var currentDate = new Date();
+        //console.log(currentDate.getDate().toString());
+        //console.log(this.taskLogs[0].taskSetDate.substring(8,10));
+        this.taskLogs = this.taskLogs.sort((a, b) => Number(b.taskSetDate.substring(8,10)) - Number(a.taskSetDate.substring(8,10)));
         this.taskLogs.forEach(log=>{
           var year = Number(log.taskSetDate.substring(0,4));
           var mnth = Number(log.taskSetDate.substring(5,7));
           var date = Number(log.taskSetDate.substring(8,10));
-          console.log(year,mnth,date);
-          console.log(currentDate.getFullYear(),currentDate.getMonth()+1,currentDate.getDate());
+          //console.log(year,mnth,date);
+          //console.log(currentDate.getFullYear(),currentDate.getMonth()+1,currentDate.getDate());
           if (currentDate.getFullYear() == year && currentDate.getDate()==date && currentDate.getMonth()+1==mnth)
             this.tasksFormed=true;
         })
 
       });
     }
+  exportexcel(): void
+  {
+    /* table id is passed over here */
+    let element = document.getElementById('excel-table');
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+
+    /* save to file */
+    XLSX.writeFile(wb, this.fileName);
+
+  }
   formTasks(){
     this.loading = true;
     this.tasksFormed = true;
